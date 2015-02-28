c0f9786e24e15129b56c6fa947998eddc3f3989a
dreammsin <dreamsin@users.noreply.github.com>
2015-02-27 23:17:01 -0600

    add hello world application

---
+++ HelloWorld/HelloWorldTest/GreetingTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld
+{
+    [TestClass]
+    public class GreetingTest
+    {
+        [TestMethod]
+        public void ToStringReturnsAGreeting()
+        {
+            //arrange
+            var instance = new Greeting();
+
+            //act
+            var results = instance.ToString();
+
+            //assert
+            StringAssert.StartsWith(results,"Hello");
+            StringAssert.EndsWith(results, "World");
+        }
+    }
+}
